2fa7098274533cfb38169f387c0afd81dca9d5e0
César Córdoba <cordobacesar29@gmail.com>
2021-08-27 12:00:38 -0300

    footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Flex } from '@chakra-ui/layout';
+
+export const Footer = () => {
+  return(
+    <Flex m='1rem'>
+      <h1>Footer</h1>
+    </Flex>
+    
+  )
+};
