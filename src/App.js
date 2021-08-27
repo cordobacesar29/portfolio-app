7de94be9bd53bbd81b0e1a4690fa55888c2ebc55
César Córdoba <cordobacesar29@gmail.com>
2021-08-27 11:59:26 -0300

    create routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
+import { Flex } from '@chakra-ui/layout';
+
+import { Home } from './pages/home';
+import { Proyects } from './pages/proyects';
+import { RoadMap } from './pages/roadMap';
+import { SignIn } from './pages/signIn';
+import { SignUp } from './pages/signUp';
+
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+
 import './App.css';
+
+const routes = [
+  { path: '/', Component: Home },
+  { path: '/proyects', Component: Proyects },
+  { path: '/roadMap', Component: RoadMap },
+  { path: '/signIn', Component: SignIn },
+  { path: '/signUp', Component: SignUp }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      {routes.map(({ path, Component }) => (
+        <Route key={path} exact path={path}>
+          {({ match }) => (
+            <CSSTransition
+              classNames='page'
+              in={match != null}
+              timeout={500}
+              unmountOnExit
+            >
+              <Flex className='page' m='1rem' justify='center' align='center'>
+                <Component />
+              </Flex>
+            </CSSTransition>
+          )}
+        </Route>
+      ))}
+      <Footer />
+    </>
   );
 }
 
