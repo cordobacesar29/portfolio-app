995297957ffc2e220bf871c8f1c9d0e83840374c
César Córdoba <cordobacesar29@gmail.com>
2021-11-03 20:31:28 -0300

    add authstatechange of firebase

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import './App.css';
 
 import { Home } from './pages/home';
 import { Proyects } from './pages/proyects';
 import { RoadMap } from './pages/roadMap';
-import { SignIn } from './pages/signIn';
 import { SignUp } from './pages/signUp';
 import { Layout } from './components/layout';
 
-import './App.css';
+import firebaseApp from './firebase';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+const auth = getAuth(firebaseApp);
 
 function App() {
+  const [globalUser, setGlobalUser] = React.useState(null);
+
+  onAuthStateChanged(auth, (userFirebase) =>{
+    if(userFirebase) {
+      setGlobalUser(userFirebase);
+    } else {
+      setGlobalUser(null);
+    }
+  });
+
   return (
     <Layout>
       <Switch>
         <Route exact path='/'> 
-          <Home/>
+          { globalUser ? <Home/> : <SignUp/> }
         </Route>  
         <Route path='/proyects'>
           <Proyects/>
         </Route> 
         <Route path='/roadMap'>
           <RoadMap/>
-        </Route>
-        <Route path='/signIn'>
-          <SignIn/>
-        </Route>
-        <Route path='/signUp'>
-          <SignUp/>
         </Route>
       </Switch>
     </Layout>
