57ba6487ac41342f66df7391e14f15a583f0b307
César Córdoba <cordobacesar29@gmail.com>
2021-11-09 14:23:52 -0300

    delete react redux provider

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import './index.css';
 import App from './App';
 
-import store from './app/store';
-
 ReactDOM.render(
-  <Provider store={store}>
     <React.StrictMode>
     <ChakraProvider>
     <Router>
       <App />
     </Router>
     </ChakraProvider>
-  </React.StrictMode>
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
