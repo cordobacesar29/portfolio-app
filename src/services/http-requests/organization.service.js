e485ea1315a06c6fc4bf11f7ec98b461e757f5c0
César Córdoba <cordobacesar29@gmail.com>
2021-11-09 14:23:27 -0300

    delete file

--- src/services/http-requests/organization.service.js
+++
@@ -1,6 +0,0 @@
-import axiosInstance from './axios.instance';
-
-const getOrganizationData = () => axiosInstance.get('organizations/1/public');
-const updateOrganizationInfo = data => axiosInstance.patch('organizations/1/public', data);
-
-export default { getOrganizationData, updateOrganizationInfo };
